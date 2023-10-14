35feb77ca9309055321da32064d4b94ff9e8107d
nongnong10 <63227388+nongnong10@users.noreply.github.com>
2023-10-14 21:55:09 +0700

    update homework

---
+++ W5/BMICalculator_edited.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class BMICalculator_edited {
+
+    public static String calculateBMI(double weight, double height) {
+        String res = "";
+        if (weight <= 0 || weight > 150 || height <= 0 || height > 3){
+            return "Invalid value";
+        }
+
+        // Calculate BMI
+        double bmi = weight / (height * height);
+
+        // Interpret the BMI
+        if (bmi < 18.5) {
+            res = "Thinness";
+        } else if (bmi >= 18.5 && bmi < 25) {
+            res = "Normal";
+        } else if (bmi >= 25.0 && bmi < 30) {
+            res = "Overweight";
+        } else if (bmi >= 30.0 && bmi < 35) {
+            res = "Obese Class I";
+        } else if (bmi >= 35.0 && bmi < 40) {
+            res = "Obese Class II";
+        } else {
+            res = "Obese Class III";
+        }
+
+        return res;
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double weight, height;
+
+        // Input weight in kilograms
+        System.out.print("Enter your weight in kilograms: ");
+        weight = scanner.nextDouble();
+
+        // Input height in meters
+        System.out.print("Enter your height in meters: ");
+        height = scanner.nextDouble();
+
+        // Print result
+        System.out.println("Your BMI is " + calculateBMI(weight, height));
+
+        scanner.close();
+    }
+}
